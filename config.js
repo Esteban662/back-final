86c72058c523e858f931d4e83127797c83661003
malu <luchiss@hotmail.com>
2020-11-13 22:22:40 -0300

    Commit Malu

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports= {
 
-    atlas_route: `mongodb+srv://La_Veloz:Guayerd.2020@cluster0.jufx9.mongodb.net/La-Veloz-Muebles?retryWrites=true&w=majority`,
+    atlas_route: `mongodb+srv://La_Veloz:Guayerd.2020@cluster0.jufx9.mongodb.net/La-Veloz?retryWrites=true&w=majority`,
     dbName: "La-Veloz-Muebles",
     dbPass: "Guayerd.2020",
     PORT: process.env.PORT ||4200,
